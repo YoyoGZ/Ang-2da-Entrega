2b439ea5717368c0cf63ff58ec44644cd66cffff
YoyoGZ <117695561+YoyoGZ@users.noreply.github.com>
2023-09-26 22:53:32 -0300

    1Lista-Jorge-Gomez

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,15 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Lista-Jorge-Gomez';
   alumnos: string [] = ['Ignacio','Agustin', 'Alejandra', 'Barbara', 'Exequiel'];
-  mrcontrol = 5;
+  mrControl = 6;
   constructor() {
     setInterval(() => {
-      this.mrcontrol++;
-    }, 1000)
+      this.mrControl++;
+    }, 2000)
   }
 // --- NgStyle ----
 
-MyStile = '3rem' 
+MyStyle = '2rem' 
 
 // ---- NgIf ----
 
@@ -25,7 +24,7 @@ EsNgIf = true;
 
 // ---- Pipes -----
 
-IsTheday = new Date();
+IsTheDate = new Date();
 
 }
 
